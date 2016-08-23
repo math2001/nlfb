2d057be7a403c2504b6b66decada6f6aab2c9293
math2001 <=>
2016-08-23 18:02:57 +1000

    init

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="./index.style.css">
+	<link rel="stylesheet" type="text/css" href="./index.highlight.css">
+	<meta name="viewport" content="width=device-width" />
+	<title>Localhost</title>
+</head>
+<body>
+	<header>
+		<a href="/" class="brand">Localhost</a>
+		<nav>
+			<ul>
+				<li><a href="/phpmyadmin/">phpmyadmin</a></li>
+				<li class="nav-sep"></li>
+				<li><a id="settings-btn">Settings</a></li>
+				<li class="nav-sep"></li>
+				<li><a href="index.server.php">Server</a></li>
+			</ul>
+		</nav>
+		<p class="search-wrapper">
+			<input type="text" id="search" class="search" placeholder="Search in this directory..." autocomplete="off">
+		</p>
+	</header>
+	<div class="wrapper">
+		<section class="main">
+			<aside class="sidebar" id="sidebar">
+				<h3>Projects</h3>
+				<ul class="projects">
+					<li>Loading...</li>
+				</ul>
+			</aside>
+			<div class="sep" id="sep"></div>
+			<article id="main">
+			 	<div class="navbar">
+			 		<span class="navbar-btn-wrapper">
+			 			<!-- <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMCIgaGVpZ2h0PSIyMCIgdmlld0JveD0iMCAwIDIwIDIwIj4KICA8cGF0aCBmaWxsPSIjNTU1IiBkPSJNOSA0bC02IDYgNiA2IDEuNC0xLjRMNi44IDExSDE2VjlINi44bDMuNi0zLjZ6Ii8+Cjwvc3ZnPg==" alt="Move up" class="navbar-btn" id="move-up"> -->
+			 			<svg viewBox="0 0 20 20" class="navbar-btn" id="move-up">
+			 			  <path fill="#555" d="M9 4l-6 6 6 6 1.4-1.4L6.8 11H16V9H6.8l3.6-3.6z"/>
+			 			</svg>
+			 		</span>
+			 		<span class="navbar-btn-wrapper">
+			 			<svg viewBox="0 0 367.136 367.136" class="navbar-btn" id="refresh">
+			 			<path d="M336.554,86.871c-11.975-18.584-27.145-34.707-44.706-47.731L330.801,0H217.436v113.91L270.4,60.691  c40.142,28.131,65.042,74.724,65.042,124.571c0,83.744-68.13,151.874-151.874,151.874S31.694,269.005,31.694,185.262  c0-58.641,32.781-111.009,85.551-136.669l-13.119-26.979C73.885,36.318,48.315,59.1,30.182,87.494  c-18.637,29.184-28.488,62.991-28.488,97.768c0,100.286,81.588,181.874,181.874,181.874s181.874-81.588,181.874-181.874  C365.442,150.223,355.453,116.201,336.554,86.871z"/>
+			 			</svg>
+			 		</span>
+			 		<span class="navbar-btn-wrapper">
+			 			<svg viewBox="0 0 1000 1000" class="navbar-btn" id="open-in-browser">
+			 			<path d="M500,390.3l216.9,219.5H553.6v326.7H446.4V609.7H283L500,390.3z M880.2,63.6c30.6,0,56.6,11.1,77.9,33.2c21.3,22.1,31.9,47.6,31.9,76.5v653.3c0,28.9-11.1,54.4-33.2,76.6c-22.1,22.1-47.6,33.2-76.5,33.2H663.4V826.6h216.9V283H119.8v543.6h216.9v109.8H119.8c-30.6,0-56.6-11.1-77.9-33.2C20.6,881.1,10,855.6,10,826.6V173.3c0-28.9,10.6-54.4,31.9-76.5c21.3-22.1,47.2-33.2,77.9-33.2H880.2L880.2,63.6z"/>
+			 			</svg>
+
+			 		</span>
+					<ul class="breadcrumb">
+						<li class="mute">Loading...</li>
+					</ul>
+			 	</div>
+				<ul class="items">
+					Loading...
+				</ul>
+			</article>
+		</section>
+
+		<div class="modal" id="settings">
+			<div class="modal-header">
+				<h3>Settings</h3>
+			</div>
+			<div class="modal-body">
+				
+			</div>
+			<div class="modal-footer">
+				<button type="button">Close</button>
+				<button type="button">Save</button>
+			</div>
+		</div>
+
+	</div>
+
+	<script type="text/javascript" src="http://localhost/tests/jquery.js"></script>
+	<script type="text/javascript" src="./highlight.js"></script>
+	<script type="text/javascript" src="./index.script.js"></script>
+	<script type="text/javascript">
+		
+	</script>
+
+	<div class="source" style="display: none;">
+		<div>Icons made by <a href="http://www.flaticon.com/authors/lucy-g" title="Lucy G">Lucy G</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+		<div>Icons made by <a href="http://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+	</div>
+</body>
+</html>
---
+++ index.server.php
@@ -0,0 +1,157 @@
+<?php
+
+
+function debug() {
+	$args = [];
+	foreach (func_get_args() as $k => $v) {
+		if (is_bool($v)) {
+			$args[] = $v ? 'true' : 'false';
+		} else {
+			$args[] = print_r($v, true);
+		}
+	}
+	echo '<pre>'.print_r(implode(' ', $args), true).'</pre>';
+}
+
+function quote($el) {
+	return '"'.$el.'"'; 
+}
+
+function is_ajax() {
+	return true or isset($_SERVER['HTTP_X_REQUESTED_WITH']); 
+}
+
+function array_default_value(&$arr, $key, $val) {
+	if (!array_key_exists($key, $arr) or $arr[$key] == '') {
+		$arr[$key] = $val;
+	}
+	return $arr;
+}
+
+function puts() {
+	foreach (func_get_args() as $k => $v) {
+		echo debug($v);
+	}
+}
+
+
+function pre() {
+	echo "<pre>"; 
+}
+
+function path_join() {
+	return implode(DIRECTORY_SEPARATOR, func_get_args()); 
+}
+
+function is_not_in() {
+	$val = func_get_arg(0);
+	$args = array_slice(func_get_args(), 1);
+	foreach ($args as $k => $v) {
+		if ($v == $val) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function listdir($path) {
+	if (is_dir($path)) {
+		$items = [
+			"dirs" => [],
+			"files" => [],
+			"errors" => []
+		];
+		if ($handle = opendir($path)) {
+		    while (false !== ($entry = readdir($handle))) {
+		    	if (is_dir(path_join($path, $entry))) {
+		    		if (is_not_in($entry, '.', '..')) {
+			    		$items['dirs'][] = $entry;
+		    		}
+		    	} elseif (is_file(path_join($path, $entry))) {
+		    		$items['files'][] = $entry;
+		    	} else {
+		    		$items['errors'][] = $entry;
+		    	}
+		    }
+
+		    closedir($handle);
+		}
+		return $items;
+	} elseif (is_file($path)) {
+		$text = file_get_contents($path);
+		if (
+			in_array('extension', array_keys($pathinfo = pathinfo($path))) AND
+			in_array(strtolower($pathinfo['extension']), ['jpg', 'png', 'bmp', 'jpeg', 'gif', 'ico']
+		)) {
+			header('content-type: image/png');
+			return $path;
+		} elseif ($text == '') {
+			return 'EMPTY';
+		} else {
+			return htmlspecialchars($text);
+		}
+	} else {
+		return "Error: what the hell?! \"$path\"";
+	}
+}
+
+function has_dirs($path) {
+	if ($handle = opendir($path)) {
+		while (false !== ($entry = readdir($handle))) {
+			if (is_dir(path_join($path, $entry)) and !in_array($entry, ['.', '..'])) {
+				return true;
+			}
+		}
+
+		closedir($handle);
+	}
+	return false;
+}
+
+function list_dirs_with_dirs_in($path) {
+	if (!is_dir($path)) {
+		return 'it is not a dir';
+	}
+	$dirs = [];
+	if ($handle = opendir($path)) {
+		while (false !== ($entry = readdir($handle))) {
+			if (is_dir(path_join($path, $entry)) and !in_array($entry, ['.', '..'])) {
+				$dirs[$entry] = has_dirs(path_join($path, $entry));
+			}
+		}
+
+		closedir($handle);
+	}
+	return $dirs;
+}
+
+if (!is_ajax()) {
+	echo "<a href='/'>Localhost</a>";
+}
+
+array_default_value($_GET, 'path', './');
+array_default_value($_GET, 'forsidebar', 'no');
+
+$_GET['path'] = '../'.$_GET['path'];
+
+$items = listdir($_GET['path']);
+
+// for the side bar
+
+if ($_GET['forsidebar'] == 'yes') {
+	$items = list_dirs_with_dirs_in($_GET['path']);
+	header('content-type: application/json');
+	die(json_encode($items));
+}
+
+// for the normal navigation
+
+if (is_array($items)) {
+	header('content-type: application/json');
+	$json = json_encode($items);
+} else {
+	$json = $items;
+}
+
+die($json);
+?>
